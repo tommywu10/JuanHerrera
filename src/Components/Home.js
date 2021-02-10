0dde3f63209d8e4997ffb8777719015e62f8118a
tommywu10 <tomwu10@gmail.com>
2021-02-09 19:25:50 -0500

    home page additions

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
+import {FaAngleDoubleDown} from "react-icons/fa"
 import recitalImage from '../images/recital.jpg'
 
 export default function Home(){
@@ -13,11 +14,26 @@ export default function Home(){
                 <div className="landing-page-text">
                     <h1>Juan Herrera</h1>
                     <h2>Percussion</h2>
+                    <FaAngleDoubleDown id="landing-page-arrow"/>
                 </div>
             </div>
 
-            <Header />
-            <img src={recitalImage} alt="Juan's recital" />
+            
+
+            <div className="home-main-container">
+                <Header />
+                
+                <div className="home-main-content">
+                    <div className="home-text-left">
+                        <p>Performer</p>
+                        <p>Teacher</p>
+                        <p>Innovator</p>
+                    </div>
+                    <img src={recitalImage} alt="Juan's recital" />
+
+                </div>
+                
+            </div>
             <Footer />
         </div>
     )
