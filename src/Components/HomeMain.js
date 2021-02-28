d185a0cb9aa64e15335ff646ed0f9b8f9fa0cccf
tommywu10 <tomwu10@gmail.com>
2021-02-27 21:34:57 -0500

    changes to home page text

--- src/Components/HomeMain.js
+++ src/Components/HomeMain.js
@@ -7,13 +7,12 @@ const HomeMain = () => {
         <div className="home-main-container" id="home-main">
             <Header />
                 <div className="home-main-content">
-                    <div className="home-main-text">
-                        <p>Performer</p>
-                        <p>Teacher</p>
-                        <p>Innovator</p>
-                    </div>
                     <img src={recitalImage} alt="Juan's recital" />
-
+                </div>
+                <div className="home-main-text">
+                    <p>Performer</p>
+                    <p>Teacher</p>
+                    <p>Innovator</p>
                 </div>
             </div>
     )
